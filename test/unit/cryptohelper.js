c7b2c8ba293b62b4ec5dcc9a935c12a58d4abe5d
Atif Nazir <atifn@atifn.net>
2021-06-07 05:23:20 +0000

    add support for dynamic decryption algorithms + version bump

--- test/unit/cryptohelper.js
+++ test/unit/cryptohelper.js
@@ -95,6 +95,39 @@ test('Encrypt using AES-256-GCM', t => {
 
 });
 
+test("DynamicExtractKey using AES-256-ECB", t => {
+    t.plan(2)
+
+    let user_key = JSON.parse('{"encrypted_passphrase":"3wIJtPoC8KO6S7x6LtrN0g==","public_key":"02f87f787bffb30396984cb6b3a9d6830f32d5b656b3e39b0abe4f3b3c35d99323","algorithm":{"pbkdf2_salt":"","pbkdf2_iterations":2048,"pbkdf2_hash_function":"SHA256","pbkdf2_phase1_key_length":16,"pbkdf2_phase2_key_length":32,"aes_iv":null,"aes_cipher":"AES-256-ECB","aes_auth_tag":null,"aes_auth_data":null}}');
+    
+    t.doesNotThrow(() => {
+	let key = CryptoHelper.dynamicExtractKey(user_key, "deadbeef");
+	t.equal(key.pub.toString('hex'), user_key.public_key, 'must return correct public key');
+    }, undefined, 'does not throw any Errors');
+});
+
+test("DynamicExtractKey using AES-256-CBC", t => {
+    t.plan(2)
+
+    let user_key = JSON.parse('{"encrypted_passphrase":"LExu1rUAtIBOekslc328Lw==","public_key":"02f87f787bffb30396984cb6b3a9d6830f32d5b656b3e39b0abe4f3b3c35d99323","algorithm":{"pbkdf2_salt":"922445847c173e90667a19d90729e1fb","pbkdf2_iterations":500000,"pbkdf2_hash_function":"SHA256","pbkdf2_phase1_key_length":16,"pbkdf2_phase2_key_length":32,"aes_iv":"11bc22166c8cf8560e5fa7e5c622bb0f","aes_cipher":"AES-256-CBC","aes_auth_tag":null,"aes_auth_data":null}}');
+    
+    t.doesNotThrow(() => {
+	let key = CryptoHelper.dynamicExtractKey(user_key, "deadbeef");
+	t.equal(key.pub.toString('hex'), user_key.public_key, 'must return correct public key');
+    }, undefined, 'does not throw any Errors');
+});
+
+test("DynamicExtractKey using AES-256-GCM", t => {
+    t.plan(2)
+
+    let user_key = JSON.parse('{"encrypted_passphrase":"ELV56Z57KoA=","public_key":"02f87f787bffb30396984cb6b3a9d6830f32d5b656b3e39b0abe4f3b3c35d99323","algorithm":{"pbkdf2_salt":"922445847c173e90667a19d90729e1fb","pbkdf2_iterations":500000,"pbkdf2_hash_function":"SHA256","pbkdf2_phase1_key_length":16,"pbkdf2_phase2_key_length":32,"aes_iv":"a57414b88b67f977829cbdca","aes_cipher":"AES-256-GCM","aes_auth_tag":"adeb7dfe53027bdda5824dc524d5e55a","aes_auth_data":""}}');
+    
+    t.doesNotThrow(() => {
+	let key = CryptoHelper.dynamicExtractKey(user_key, "deadbeef");
+	t.equal(key.pub.toString('hex'), user_key.public_key, 'must return correct public key');
+    }, undefined, 'does not throw any Errors');
+});
+
 test('Satoshis from value string', t => {
   t.plan(4);
 
