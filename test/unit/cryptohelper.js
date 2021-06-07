97208819cd070365eab5c69589dca93c98136927
Atif Nazir <atifn@atifn.net>
2021-06-07 04:59:15 +0000

    WIP: add support for AES-256-CBC and AES-256-GCM

--- test/unit/cryptohelper.js
+++ test/unit/cryptohelper.js
@@ -5,6 +5,7 @@ const PIN = '123456';
 const PIN_KEY = '0EeMOVtm5YihUYzdCNgleqIUWkwgvNBcRmr7M0t9GOc=';
 const CLEARTEXT = 'I\'m a little tea pot short and stout';
 const CIPHERTEXT = '7HTfNBYJjq09+vi8hTQhy6lCp3IHv5rztNnKCJ5RB7cSL+NjHrFVv1jl7qkxJsOg';
+const PIN_KEY_500000 = CryptoHelper.pinToKey("deadbeef", "922445847c173e90667a19d90729e1fb", 500000);
 
 test('Deriving a PIN into an AES key', t => {
   t.plan(2);
@@ -20,28 +21,75 @@ test('Deriving a PIN into an AES key with Salt', t => {
   t.plan(2);
 
     t.doesNotThrow(() => {
-	// (pin, salt, iterations, hash_function, phase1_key_length, phase2_key_length)
-	const key = CryptoHelper.pinToKey("deadbeef", "922445847c173e90667a19d90729e1fb", 500000);
-	t.equal(Buffer.from(key, "base64").toString("hex"), "f206403c6bad20e1c8cb1f3318e17cec5b2da0560ed6c7b26826867452534172", 'must return the correct derived key');
+	t.equal(Buffer.from(PIN_KEY_500000, "base64").toString("hex"), "f206403c6bad20e1c8cb1f3318e17cec5b2da0560ed6c7b26826867452534172", 'must return the correct derived key');
   }, undefined, 'must not throw any Errors');
 
 });
 
-test('Encrypting some data', t => {
+test('Encrypt using AES-256-ECB', t => {
   t.plan(2);
 
   let enc;
   t.doesNotThrow(() => {
     enc = CryptoHelper.encrypt(CLEARTEXT, PIN_KEY);
-    t.equal(enc, CIPHERTEXT, 'must return the correct ciphertext');
+    t.equal(enc.aes_cipher_text, CIPHERTEXT, 'must return the correct ciphertext');
   }, undefined, 'does not throw any Errors');
 
-  test('Decrypting the encrypted data', t => {
+  test('Decrypt using AES-256-ECB', t => {
     t.plan(2);
     t.doesNotThrow(() => {
-      const dec = CryptoHelper.decrypt(enc, PIN_KEY)
+      const dec = CryptoHelper.decrypt(enc.aes_cipher_text, PIN_KEY)
       t.equal(dec, CLEARTEXT, 'must return the correct cleartext');
     }, undefined, 'does not throw any Errors');
+
+  });
+
+});
+
+test('Encrypt using AES-256-CBC', t => {
+  t.plan(2);
+
+  let enc;
+  t.doesNotThrow(() => {
+      enc = CryptoHelper.encrypt("beadbeef", PIN_KEY_500000, "11bc22166c8cf8560e5fa7e5c622bb0f", "AES-256-CBC");
+    t.equal(enc.aes_cipher_text, "LExu1rUAtIBOekslc328Lw==", 'must return the correct ciphertext');
+  }, undefined, 'does not throw any Errors');
+
+  test('Decrypt using AES-256-CBC', t => {
+    t.plan(2);
+    t.doesNotThrow(() => {
+	const dec = CryptoHelper.decrypt(enc.aes_cipher_text, PIN_KEY_500000, "11bc22166c8cf8560e5fa7e5c622bb0f", "AES-256-CBC")
+      t.equal(dec, "beadbeef", 'must return the correct cleartext');
+    }, undefined, 'does not throw any Errors');
+
+  });
+
+});
+
+test('Encrypt using AES-256-GCM', t => {
+  t.plan(3);
+
+  let enc;
+  t.doesNotThrow(() => {
+      enc = CryptoHelper.encrypt("beadbeef", PIN_KEY_500000, "a57414b88b67f977829cbdca", "AES-256-GCM");
+    t.equal(enc.aes_cipher_text, "ELV56Z57KoA=", 'must return the correct ciphertext');
+    t.equal(enc.aes_auth_tag, "adeb7dfe53027bdda5824dc524d5e55a", 'must return the correct auth_tag');
+  }, undefined, 'does not throw any Errors');
+
+  test('Decrypt using AES-256-GCM', t => {
+    t.plan(2);
+    t.doesNotThrow(() => {
+	const dec = CryptoHelper.decrypt(enc.aes_cipher_text, PIN_KEY_500000, "a57414b88b67f977829cbdca", "AES-256-GCM", enc.aes_auth_tag, enc.aes_auth_data)
+      t.equal(dec, "beadbeef", 'must return the correct cleartext');
+    }, undefined, 'does not throw any Errors');
+
+  });
+
+  test('Decrypt using AES-256-GCM with small auth tag', t => {
+    t.plan(1);
+    t.throws(() => {
+	CryptoHelper.decrypt(enc.aes_cipher_text, PIN_KEY_500000, "a57414b88b67f977829cbdca", "AES-256-GCM", enc.aes_auth_tag.slice(0,30), enc.aes_auth_data);
+    }, "Auth tag must be 16 bytes exactly.", 'throws an Error');
 
   });
 
